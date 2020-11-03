aba5951ee40c21f179aae1b6418aedf3702c84e8
yooperjb <jmb181@humboldt.edu>
2020-11-03 15:58:10 -0800

    initial files and js setup

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+
+
+require([ "esri/Map", "esri/views/MapView" ], function(Map, MapView) {
+    // Code to create the map and view will go here
+    var map = new Map({
+        basemap: "topo-vector"
+    });
+  });
