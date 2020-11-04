aa6ffe24da975e544e7127eb84eac7586e3588ef
Pedal <jmb181@humboldt.edu>
2020-11-04 00:18:09 -0800

    map created and toggle created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,58 @@
 
 
+require([ 
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/TileLayer" ], 
+    function(Map, MapView, TileLayer) {
+    
+    var transportationLayer = new TileLayer({
+        url: "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer",
+        id: "streets",
+        opacity: 0.7
+    });
 
-require([ "esri/Map", "esri/views/MapView" ], function(Map, MapView) {
+    var housingLayer = new TileLayer({
+        url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/New_York_Housing_Density/MapServer",
+        id: "ny-housing"
+      });
+
+    
     // Code to create the map and view will go here
     var map = new Map({
-        basemap: "topo-vector"
+        basemap: "topo-vector",
+        layers: [housingLayer, transportationLayer] // Layers can be added as an arrary
     });
-  });
+    
+    var view = new MapView({
+        container: "viewDiv", // Reference to the DOM node that will contain the view
+        map: map // References the map object created in step 3
+      });
+    
+    // can also add layers with map.layers.add
+    //map.layers.add(transportationLayer);
+    var streetsLayerToggle = document.getElementById("streetsLayer");
+    
+    // Listen to the change event for the checkbox
+    streetsLayerToggle.addEventListener("change", function(){
+        // When the checkbox is checked (true), set the layer's visibility to true
+        transportationLayer.visible = streetsLayerToggle.checked;
+    });
+    
+    
+    view.on("layerview-create", function(event) {
+        if (event.layer.id === "ny-housing") {
+          // Explore the properties of the housing layer's layer view here
+          console.log("LayerView for New York housing density created!", event.layerView);
+        }
+        if (event.layer.id === "streets") {
+          // Explore the properties of the transportation layer's layer view here
+          //console.log("LayerView for streets created!", event.layerView);
+        }
+    });
+
+    // When the layer's promise resolves, animate the view to the layer's fullExtent
+    housingLayer.when(function() {
+        view.goTo(housingLayer.fullExtent);
+    });
+});
