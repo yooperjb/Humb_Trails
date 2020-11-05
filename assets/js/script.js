d7117b039125b1e0e4306b825743d78b5b0b50b2
yooperjb <jmb181@humboldt.edu>
2020-11-05 15:58:58 -0800

    added arcgisonline feature layer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@
 require([ 
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/TileLayer" ], 
-    function(Map, MapView, TileLayer) {
+    "esri/layers/TileLayer",
+    "esri/layers/FeatureLayer" ], 
+    function(Map, MapView, TileLayer, FeatureLayer) {
     
     var transportationLayer = new TileLayer({
         url: "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer",
@@ -16,12 +17,22 @@ require([
         url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/New_York_Housing_Density/MapServer",
         id: "ny-housing"
       });
+    
+    // points to a hosted Feature Layer in ArcGIS Online (Eureka Trustee Areas)
+    const trustee = new FeatureLayer({
+        portalItem: {  // autocasts as esri/portal/PortalItem
+        id: "7e6db93006ff4b30ac401871d2cf3991"
+      }
+    });
+
+    //map.add(fl);  // adds the layer to the map
+
 
     
     // Code to create the map and view will go here
     var map = new Map({
         basemap: "topo-vector",
-        layers: [housingLayer, transportationLayer] // Layers can be added as an arrary
+        layers: [housingLayer, transportationLayer,trustee] // Layers can be added as an arrary
     });
     
     var view = new MapView({
@@ -52,7 +63,7 @@ require([
     });
 
     // When the layer's promise resolves, animate the view to the layer's fullExtent
-    housingLayer.when(function() {
-        view.goTo(housingLayer.fullExtent);
+    trustee.when(function() {
+        view.goTo(trustee.fullExtent);
     });
 });
